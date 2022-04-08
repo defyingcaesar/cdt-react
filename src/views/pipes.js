20b08bfe8d432e3271cdf420db5b036c317246d7
jethro <jethro@echdesign.co.uk>
2022-04-08 11:39:34 +0200

    Changed button layout

--- src/views/pipes.js
+++ src/views/pipes.js
@@ -11,7 +11,7 @@ const Pipes = () => {
     <div>
       <h2>Pipes And Release</h2>
         <div className="row">
-        <NavLink to="/pipe-one" className={"btn btn-primary btn-lg"}>Pipe One</NavLink>
+        <NavLink to="/pipe-one" className={"btn btn-primary btn-lg btn-block"}>Pipe One</NavLink>
         <NavLink to="/pipe-two" className={"btn btn-primary btn-lg btn-block"}>Pipe Two</NavLink>
         <NavLink to="/pipe-three" className={"btn btn-primary btn-lg btn-block"}>Pipe Three</NavLink>
         <NavLink to="/pipe-four" className={"btn btn-primary btn-lg btn-block"}>Pipe Four</NavLink>
