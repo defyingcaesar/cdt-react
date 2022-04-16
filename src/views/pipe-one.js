a003ef0aeda16a355325ca91d5a009831eb46039
defyingcaesar <jethrodw@protonmail.com>
2022-04-16 23:14:56 +0200

    changed height on bar graph pipe one

--- src/views/pipe-one.js
+++ src/views/pipe-one.js
@@ -21,7 +21,7 @@ const PipeOne = () => {
       </div>
       <div className="row">
         <div className="col-md-12 col-sm-12 mb-3">
-          <iframe src="https://grafana.comdatech.xyz/d-solo/QrfhicPnk/da-gama-pipes?orgId=1&refresh=15m&from=now&to=now-7d&panelId=17" width="100%" height="100%" frameborder="0" title="Pipe 1 Acc"></iframe>
+          <iframe src="https://grafana.comdatech.xyz/d-solo/QrfhicPnk/da-gama-pipes?orgId=1&refresh=15m&from=now&to=now-7d&panelId=17" width="100%" height="350" frameborder="0" title="Pipe 1 Acc"></iframe>
         </div>
       </div>
     </div>
