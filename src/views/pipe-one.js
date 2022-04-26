a70558aac227cb1828c7ad9b5adc9fc48968f322
jethro <jethro@echdesign.co.uk>
2022-04-26 12:57:33 +0200

    Added range picker components

--- src/views/pipe-one.js
+++ src/views/pipe-one.js
@@ -1,16 +1,25 @@
 import React from "react";
 
+import Dropdown from "../components/dropdown";
+
 // import { useAuth0 } from "@auth0/auth0-react";
 
 const PipeOne = () => {
   
-  const options = [
-    { label: '1d', value: 'now=now-1d' },
-    { label: '2d', value: 'to=now-2d' },
-    { label: '3d', value: 'to=now-3d' },
+const options = [
+    { label: '1d', value: 'now-1d' },
+    { label: '2d', value: 'now-2d' },
+    { label: '3d', value: 'now-3d' },
+    { label: '4d', value: 'now-4d' },
+    { label: '5d', value: 'now-5d' },
+    { label: '6d', value: 'now-6d' },
+    { label: '7d', value: 'now-7d' },
+    { label: '15d', value: 'now-15d' },
+    { label: '30d', value: 'now-30d' },
+  
   ];
 
-  const [value, setValue] = React.useState('fruit');
+  const [value, setValue] = React.useState('now-7d');
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -23,6 +32,7 @@ const PipeOne = () => {
         value={value}
         onChange={handleChange}
       />
+      <p>Current time range is {value}!</p>
         <h2>Pipe One</h2>
           <div className="row">
             <div className="col-md-12 col-sm-12 mb-3">
@@ -31,27 +41,15 @@ const PipeOne = () => {
           </div>
           <div className="row">
             <div className="col-md-12 col-sm-12 mb-3">
-              <iframe src="https://grafana.comdatech.xyz/d-solo/QrfhicPnk/da-gama-pipes?orgId=1&refresh=15m&from=now&to=now-7d&panelId=8" width="100%" height="100%" frameborder="0" title="Pipe 1 Acc 24h"></iframe>
+              <iframe src={`https://grafana.comdatech.xyz/d-solo/QrfhicPnk/da-gama-pipes?orgId=1&refresh=15m&from=now&to=${value}&panelId=8`} width="100%" height="100%" frameborder="0" title="Pipe 1 Acc 24h"></iframe>
             </div>
           </div>
           <div className="row">
             <div className="col-md-12 col-sm-12 mb-3">
-              <iframe src={`https://grafana.comdatech.xyz/d-solo/QrfhicPnk/da-gama-pipes?orgId=1&refresh=15m&from=now&${options.value}&panelId=18`} width="100%" height="350" frameborder="0" title="Pipe 1 History"></iframe>
+              <iframe src={`https://grafana.comdatech.xyz/d-solo/QrfhicPnk/da-gama-pipes?orgId=1&refresh=15m&from=now&to=${value}&panelId=18`} width="100%" height="350" frameborder="0" title="Pipe 1 History"></iframe>
             </div>
           </div>
     </div>
-  );
-};
-const Dropdown = ({ label, value, options, onChange }) => {
-  return (
-    <label>
-      {label}
-      <select value={value} onChange={onChange}>
-        {options.map((option) => (
-          <option value={option.value}>{option.label}</option>
-        ))}
-      </select>
-    </label>
   );
 };
 
