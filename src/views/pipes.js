0d9e7c3e304063c328dd9510087e8fc680faf512
jethro <jethro@echdesign.co.uk>
2022-05-19 14:39:34 +0200

    Added all pipes to page

--- src/views/pipes.js
+++ src/views/pipes.js
@@ -10,6 +10,11 @@ const Pipes = () => {
   return (
     <div>
       <h2>Pipes And Release</h2>
+      <div className="row">
+      <div className="col-md-4 col-sm-12 mb-3">
+        <iframe src="https://grafana.comdatech.xyz/d-solo/u_11G1Pnz/calibration-dashboard?orgId=1&from=now&to=now1&panelId=54" width="1000" height="200" frameBorder="0" title="dam-pipes"></iframe>
+      </div>
+      </div>
         <div className="row">
         <NavLink to="/pipe-one" className={"btn btn-primary btn-lg btn-block"}>Pipe One</NavLink>
         <NavLink to="/pipe-two" className={"btn btn-primary btn-lg btn-block"}>Pipe Two</NavLink>
